3fda1783f964daed686821823dd1a0cb1500c615
Slappis <57948518+Slappis@users.noreply.github.com>
2021-02-25 15:56:41 +0100

    Regestrering
    Det går nu att regestrera sig efter lite förändringar inom databasen

--- app.rb
+++ app.rb
@@ -3,7 +3,31 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 enable :sessions
+db = SQLite3::Database.new('db/WSP21slutprojekt.db')
 
 get('/') do
     slim(:register)
+end
+
+get('/showlogin') do
+    slim(:login)
+end
+
+post('/user/new') do
+    # Från register.slim formuläret
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+  
+    if (password == password_confirm)
+      #Lägg till användare
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/WSP21slutprojekt.db')
+      db.execute("INSERT INTO users (username,password) VALUES (?,?)",username,password_digest)
+      redirect('/')
+  
+    else
+      #Felhantering
+      "Lösenorden matchade inte"
+    end
   end
