d1f42d271a614f3ff1aa652f9dbdd52801cd64cb
Slappis <57948518+Slappis@users.noreply.github.com>
2021-02-25 16:09:44 +0100

    Login
    Nu fungerar login funktionen och jag har en tomporär "hem" skärm

--- app.rb
+++ app.rb
@@ -13,6 +13,10 @@ get('/showlogin') do
     slim(:login)
 end
 
+get('/home') do
+    slim(:home)
+end
+
 post('/user/new') do
     # Från register.slim formuläret
     username = params[:username]
@@ -22,12 +26,36 @@ post('/user/new') do
     if (password == password_confirm)
       #Lägg till användare
       password_digest = BCrypt::Password.create(password)
-      db = SQLite3::Database.new('db/WSP21slutprojekt.db')
       db.execute("INSERT INTO users (username,password) VALUES (?,?)",username,password_digest)
       redirect('/')
   
     else
       #Felhantering
       "Lösenorden matchade inte"
+    end
+  end
+
+  post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db.results_as_hash = true
+  
+    allUsers = db.execute("SELECT username FROM users")
+  
+    if allUsers.include?({"username" => username})
+      result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+      pwdigest = result["password"]
+      id = result["id"]
+  
+      if BCrypt::Password.new(pwdigest) == password 
+        session[:id] = id
+        redirect('/home')
+         
+      else
+        "Fel Lösenord" 
+      end
+  
+    else
+      "Användarnamnet existerar inte"
     end
   end
